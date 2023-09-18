1d02549481d1f1cc51be0fc8d5dcd9276f362819
ShubhayanDatta <dattashubhayan1@gmail.com>
2023-09-18 17:51:49 +0530

    added database and modified server

---
+++ backend/schema.sql
@@ -0,0 +1,8 @@
+create table BOOKS
+{
+    book_id int primary key,
+    book_name varchar(30),
+    author varchar(30),
+    summary text,
+    genre text,
+};
--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,18 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
    return "hello world"
+@app.route('/display')
+def show_database():
+   return "1" 
 
 if __name__ == '__main__':
    app.run()
